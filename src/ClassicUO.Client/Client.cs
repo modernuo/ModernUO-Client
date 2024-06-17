6d05cb732ad88abfba6ed39ad49b4165887ad082
Max Kellermann <max.kellermann@gmail.com>
2024-06-17 17:46:38 +0200

    Client/UltimaLive: delete
    This feature is not supported by the ModernUO-Client project.
    
    Closes https://github.com/modernuo/ModernUO-Client/issues/14

--- src/ClassicUO.Client/Client.cs
+++ src/ClassicUO.Client/Client.cs
@@ -241,8 +241,6 @@ namespace ClassicUO
             ChairTable.Load();
 
             Log.Trace("Network calibration...");
-            //ATTENTION: you will need to enable ALSO ultimalive server-side, or this code will have absolutely no effect!
-            UltimaLive.Enable();
             PacketsTable.AdjustPacketSizeByVersion(Version);
 
             if (Settings.GlobalSettings.Encryption != 0)
