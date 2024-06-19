bc70e60f734b8d2032bd9a65e0173e2415abc381
Max Kellermann <max.kellermann@gmail.com>
2024-06-19 10:07:46 +0200

    Revert "+ UOLive needs hashes! we need to find out a better solution, but keep 'em for the moment"
    This reverts commit bd0c803d0d11ac295d4cfa0aebbc9c1ca9a167ac.
    UltimaLive was removed, and thus we can now free some memory.

--- src/ClassicUO.Assets/MapLoader.cs
+++ src/ClassicUO.Assets/MapLoader.cs
@@ -228,8 +228,7 @@ namespace ClassicUO.Assets
 
             if (isuop)
             {
-                // TODO: UOLive needs hashes! we need to find out a better solution, but keep 'em for the moment
-                //((UOFileUop)file)?.ClearHashes();
+                ((UOFileUop)file)?.ClearHashes();
             }
         }
 
