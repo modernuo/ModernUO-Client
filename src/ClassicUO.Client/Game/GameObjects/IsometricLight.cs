03153dc35d2f46064d7b098a1a25145ff9646958
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-07-11 12:38:23 -0700

    IsometricLight: Fixes spelling of real overall variable

--- src/ClassicUO.Client/Game/GameObjects/IsometricLight.cs
+++ src/ClassicUO.Client/Game/GameObjects/IsometricLight.cs
@@ -2,7 +2,7 @@
 
 // Copyright (c) 2024, andreakarasho
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // 1. Redistributions of source code must retain the above copyright
@@ -16,7 +16,7 @@
 // 4. Neither the name of the copyright holder nor the
 //    names of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -37,7 +37,7 @@ namespace ClassicUO.Game.GameObjects
     internal sealed class IsometricLight
     {
         private float _height = -0.75f;
-        private int _overall = 9, _realOveall = 9;
+        private int _overall = 9, _realOverall = 9;
         private int _personal = 9, _realPersonal = 9;
 
         public int Personal
@@ -82,10 +82,10 @@ namespace ClassicUO.Game.GameObjects
 
         public int RealOverall
         {
-            get => _realOveall;
+            get => _realOverall;
             set
             {
-                _realOveall = value;
+                _realOverall = value;
                 Recalculate();
             }
         }
